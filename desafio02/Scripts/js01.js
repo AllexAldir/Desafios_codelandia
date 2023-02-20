0cfb95b9a422b560e0512c22294332fa9c7b1f3c
Allex Aldir <114629926+AllexAldir@users.noreply.github.com>
2023-02-19 22:57:17 -0300

    Colocando interações!

---
+++ desafio02/Scripts/js01.js
@@ -0,0 +1,14 @@
+//Marcado a classe!
+window.sr = ScrollReveal({reset: true})
+
+ScrollReveal().reveal('.sub_div',{
+    delay: 400,
+    rotate:{
+        x:100,
+        y:0,
+        z:0
+    }
+    
+    //Aqui vai mostar o delay de quanto tempo irá aparecer a interatividade!
+
+});
